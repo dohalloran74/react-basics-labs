54920aa30ca7f5a09a1f72d4d548f1887b5a9b38
deirdreOH <dohalloran@wit.ie>
2024-09-16 11:18:39 +0100

    Completed Week 1 lab

--- tasky/src/App.js
+++ tasky/src/App.js
@@ -1,6 +1,21 @@
 import './App.css';
 import Task from './components/Task';
 
+
+function App() {
+    return ( 
+        <div className = "container" >
+        <h1> Tasky </h1> 
+        <Task title="Dishes" deadline="Today" description="Empty Dishwaser"/>
+        
+        <Task title = "Laundry" deadline = "Tomorrow" description="Fold laundry and put away" />
+       {/*</div> fold laundry and put away  </Task>*/}
+        <Task title = "Tidy" deadline = "Today" />
+        </div>
+    );
+}
+
+/* This is a comment in React - working code
 function App() {
     return ( 
         <div className = "container" >
@@ -12,5 +27,7 @@ function App() {
         </div>
     );
 }
+*/
+
 
 export default App;
